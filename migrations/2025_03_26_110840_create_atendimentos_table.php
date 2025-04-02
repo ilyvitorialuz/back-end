1bc8ba9dc76116f2d853b66619617cb55fae42cf
ilyvitorialuz <ily.soares@estudante.ifto.edu.br>
2025-04-02 20:29:45 -0300

    Update 2025_03_26_110840_create_atendimentos_table.php

--- migrations/2025_03_26_110840_create_atendimentos_table.php
+++ migrations/2025_03_26_110840_create_atendimentos_table.php
@@ -1,27 +1,25 @@
 <?php
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateAtendimentosTable extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
         Schema::create('atendimentos', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('pet_id')->constrained('pets')->onDelete('cascade');
+            $table->dateTime('data_atendimento');
+            $table->string('servico');
+            $table->text('observacao')->nullable();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('atendimentos');
     }
-};
+}
+?>
