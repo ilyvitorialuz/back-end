afdde7487f3c472dc844717713c7ff2f0b3890b9
ilyvitorialuz <ily.soares@estudante.ifto.edu.br>
2025-04-02 20:27:18 -0300

    Update 2025_03_26_110437_create_pets_table.php

--- migrations/2025_03_26_110437_create_pets_table.php
+++ migrations/2025_03_26_110437_create_pets_table.php
@@ -1,27 +1,27 @@
 <?php
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreatePetsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
         Schema::create('pets', function (Blueprint $table) {
             $table->id();
+            $table->string('nome');
+            $table->integer('idade');
+            $table->string('especie');
+            $table->string('raca');
+            $table->decimal('peso', 5, 2);
+            $table->foreignId('cliente_id')->constrained('clientes')->onDelete('cascade');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('pets');
     }
-};
+}
+?>
