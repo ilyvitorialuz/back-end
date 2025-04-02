7b2e9351ff10fef95e8a6e0914658572c88f8c6a
ilyvitorialuz <ily.soares@estudante.ifto.edu.br>
2025-04-02 20:26:46 -0300

    Update 2025_03_26_110237_create_clientes_table.php

--- migrations/2025_03_26_110237_create_clientes_table.php
+++ migrations/2025_03_26_110237_create_clientes_table.php
@@ -1,27 +1,25 @@
 <?php
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateClientesTable extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
         Schema::create('clientes', function (Blueprint $table) {
             $table->id();
+            $table->string('nome');
+            $table->string('email')->unique();
+            $table->string('telefone');
+            $table->text('endereco')->nullable();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('clientes');
     }
-};
+}
+?>
